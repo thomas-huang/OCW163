dd2c727b89f5044623620667754887813559efe7
Thomas Huang <hyhfreeman@gmail.com>
2014-03-30 00:31:29 +0800

    简单完成功能.

---
+++ downloader.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+
+from pyquery import PyQuery
+from multiprocessing import dummy
+import os
+import urllib2
+import chardet
+
+def task(args):
+    if os.system("wget -O %s -c %s " % (args[0].encode("utf-8"),
+                                        args[1].encode("utf-8"))) != 0:
+        exit(1)
+
+def download(url,pool):
+    #html = urllib2.urlopen(url).read().decode('GBK','ignore')
+    pq = PyQuery(url)
+    courseList = pq("#list2")
+    kv = [
+            ( i(".u-ctitle>a").text() , i(".downbtn").attr("href") )
+            for i in courseList(".u-even,.u-odd").items()
+        ]
+    print kv
+    pool.map(task, kv)
+    pool.close()
+    pool.join()
+
+
+def run():
+    while 1:
+        URL = raw_input("请输入公开课主页面的URL:")
+        POOL = dummy.Pool(5)
+        download(URL,POOL)
+        choice = raw_input("继续(y/n):")
+        if choice != "Y" or choice != "y":
+            exit(0)
+
+if __name__ == "__main__" :
+    run()
